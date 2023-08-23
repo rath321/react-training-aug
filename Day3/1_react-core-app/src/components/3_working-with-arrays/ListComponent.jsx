d8f739cf5f1d597e53daa25a4a6d6ddbcd02b45a
manish.sharma <manish.sharma@technizerindia.com>
2023-08-23 14:54:03 +0530

    Datatable

--- Day3/1_react-core-app/src/components/3_working-with-arrays/ListComponent.jsx
+++ Day3/1_react-core-app/src/components/3_working-with-arrays/ListComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DataTable from '../common/DataTable';
 
 const ListItem = ({ item }) => <li className='list-group-item'>{item.name}</li>
 
@@ -9,9 +10,54 @@ const ListComponent = (props) => {
 
     return (
         <>
+            {props.children}
+            <hr />
             <ul className="list-group">
                 {l_items}
             </ul>
+        </>
+    );
+}
+
+const Tr = ({ item }) => (
+    <tr>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>
+            <a href="/#" className='text-primary'>Details</a>
+        </td>
+        <td>
+            <a href="/#" className='text-warning'>Edit</a>
+        </td>
+        <td>
+            <a href="/#" className='text-danger'>Delete</a>
+        </td>
+    </tr>
+);
+
+const TableComponent = (props) => {
+    var trs = props.items.map((item, index) => {
+        return <Tr item={item} key={item.id} />
+    });
+
+    return (
+        <>
+            {props.children}
+            <hr />
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th></th>
+                        <th>ACTIONS</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {trs}
+                </tbody>
+            </table>
         </>
     );
 }
@@ -27,13 +73,51 @@ class ListRoot extends Component {
                 { id: 4, name: "Subodh" },
                 { id: 5, name: "Abhishek" }
             ],
+            posts: [
+                {
+                    userId: 1,
+                    id: 1,
+                    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                },
+                {
+                    userId: 1,
+                    id: 2,
+                    title: "qui est esse",
+                    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+                }
+            ]
         }
     }
 
     render() {
         return (
             <div>
-                <ListComponent items={this.state.employees} />
+                {/* <ListComponent items={this.state.employees}>
+                    <h4 className='text-info'>Employees Data</h4>
+                </ListComponent>
+
+                <div className="mt-5"></div>
+
+                <TableComponent items={this.state.employees}>
+                    <h4 className='text-info'>Employees Table</h4>
+                </TableComponent>
+
+                <div className="mt-5"></div>
+
+                <TableComponent items={this.state.posts}>
+                    <h4 className='text-info'>Posts Table</h4>
+                </TableComponent> */}
+
+                <DataTable items={this.state.employees}>
+                    <h4 className='text-info'>Employees Table</h4>
+                </DataTable>
+
+                <div className="mt-5"></div>
+
+                <DataTable items={this.state.posts}>
+                    <h4 className='text-info'>Employees Table</h4>
+                </DataTable>
             </div>
         );
     }
---
+++ Day3/1_react-core-app/src/components/common/DataTable.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Th = ({ item }) => {
+    var allHeads = Object.keys(item).concat("", "actions", "");
+
+    var ths = allHeads.map((head, index) => {
+        return <th key={index}>{head.toUpperCase()}</th>
+    });
+
+    return (
+        <tr>
+            {ths}
+        </tr>
+    );
+}
+
+const Tr = ({ item }) => {
+    var allValues = Object.values(item).concat(
+        <a href="/#" className='text-primary'>Details</a>,
+        <a href="/#" className='text-warning'>Edit</a>,
+        <a href="/#" className='text-danger'>Delete</a>
+    );
+
+    var tds = allValues.map((item, index) => {
+        return <td key={index}>{item}</td>
+    });
+
+    return (
+        <tr>
+            {tds}
+        </tr>
+    );
+}
+
+const DataTable = ({ items, children }) => {
+    if (items && items.length) {
+        var [item] = items;
+        var headers = <Th item={item} />;
+        var trs = items.map((item, index) => {
+            return <Tr item={item} key={item.id} />
+        });
+    }
+    return (
+        <table className='table table-striped'>
+            <thead>
+                {headers}
+            </thead>
+            <tbody>
+                {trs}
+            </tbody>
+        </table>
+    );
+};
+
+export default DataTable;
